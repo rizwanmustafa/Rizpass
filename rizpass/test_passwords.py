5913baa0183f19fbfd16f215c7dd635f445f226e
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-05-13 18:14:32 +0500

    Tests for follows_password_requirements]

--- rizpass/test_passwords.py
+++ rizpass/test_passwords.py
@@ -49,8 +49,73 @@ class TestPasswords(unittest.TestCase):
         self.assertEqual(pass_details["digits"], 3)
         self.assertEqual(pass_details["special"], 3)
 
-    # TODO: Write tests for testing follows_password_requirements
+    def test_follows_password_requirements_1(self):
+        """ Tests lowercase parameter """
+        password = "password"
 
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 8, 0, 0)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 16, 0, 0)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 1)
+
+    def test_follows_password_requirements_2(self):
+        """ Tests uppercase parameter """
+        password = "PASSWORD"
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 8, 0, 0, 0)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 16, 0, 0, 0)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 1)
+
+    def test_follows_password_requirements_3(self):
+        """ Tests digits parameter """
+        password = "12345678"
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 0, 8, 0)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 0, 16, 0)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 1)
+
+    def test_follows_password_requirements_4(self):
+        """ Tests special parameter """
+        password = "!@#$%^&*"
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 0, 0, 8)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 0, 0, 16)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 1)
+
+    def test_follows_password_requirements_5(self):
+        """ Tests all parameters """
+        password = "1234567890"
+        follows_requirements, errors = follows_password_requirements(password, 12, 3, 3, 3, 3)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 4)
+
+        follows_requirements, errors = follows_password_requirements(password, 8, 0, 0, 0, 0)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        password = "abcABC123!@?"
+        follows_requirements, errors = follows_password_requirements(password, 12, 3, 3, 3, 3)
+        self.assertEqual(follows_requirements, True)
+        self.assertEqual(errors, [])
+
+        follows_requirements, errors = follows_password_requirements(password, 16, 3, 3, 3, 3)
+        self.assertEqual(follows_requirements, False)
+        self.assertEqual(len(errors), 1)
 
 
 if __name__ == "__main__":
