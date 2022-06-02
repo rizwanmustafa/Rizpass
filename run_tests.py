f762f994b3b8ac0fa30e51ccf4d34acd9923713e
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-06-02 22:25:27 +0500

    Check pr action

---
+++ run_tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+from rizpass.test_better_input import *
+from rizpass.test_validator import *
+from rizpass.test_passwords import *
+from rizpass.test_credentials import *
+from rizpass.test_file_manager import *
+
+
+unittest.main()
