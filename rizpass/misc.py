b6dc8170bb496326dd61f195ec63a8e4dfa34bfe
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-06-05 01:46:28 +0500

    Bump version numbers

--- rizpass/misc.py
+++ rizpass/misc.py
@@ -3,7 +3,7 @@ from sys import stderr, stdout
 
 from .validator import ensure_type
 
-VERSION_NUMBER = 'v0.0.2-alpha'
+VERSION_NUMBER = 'v0.0.3-alpha'
 
 
 def print_help(error: bool = False) -> None:
