9f76e62e4d761417236d6437365a4e254c9511b3
Rizwan Mustafa <rizwanmustafa0000@gmail.com>
2022-06-24 21:08:14 +0500

    Rename nonce to salt

--- rizpass/better_algo.py
+++ rizpass/better_algo.py
@@ -4,7 +4,7 @@ from base64 import b64encode, b64decode
 from .validator import ensure_type
 
 
-def get_custom_key(master_pass: str, nonce: bytes) -> bytes:
+def get_custom_key(master_pass: str, salt: bytes) -> bytes:
     from cryptography.hazmat.primitives import hashes
     from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
     master_pass = bytes(master_pass, "utf-8")
@@ -13,33 +13,33 @@ def get_custom_key(master_pass: str, nonce: bytes) -> bytes:
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
-        salt=nonce,
+        salt=salt,
         iterations=100000,
     )
     return kdf.derive(master_pass)
 
 
-def generate_nonce() -> bytes:
+def generate_salt() -> bytes:
     from secrets import token_bytes
     return token_bytes(12)
 
 
-def encrypt_string(master_pass: str, raw_data: str, nonce: bytes) -> Union[bytes, None]:
+def encrypt_string(master_pass: str, raw_data: str, salt: bytes) -> Union[bytes, None]:
     from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
-    derived_key = get_custom_key(master_pass, nonce)
+    derived_key = get_custom_key(master_pass, salt)
 
-    encrypted_data = AESGCM(derived_key).encrypt(nonce, bytes(raw_data, "utf-8"), b"")
+    encrypted_data = AESGCM(derived_key).encrypt(salt, bytes(raw_data, "utf-8"), b"")
 
     return encrypted_data
 
 
-def decrypt_string(master_pass: str, encrypted_data: bytes, nonce: bytes) -> Union[str, None]:
+def decrypt_string(master_pass: str, encrypted_data: bytes, salt: bytes) -> Union[str, None]:
     from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
-    derived_key = get_custom_key(master_pass, nonce)
+    derived_key = get_custom_key(master_pass, salt)
 
-    decrypted_data = AESGCM(derived_key).decrypt(nonce, encrypted_data, b"")
+    decrypted_data = AESGCM(derived_key).decrypt(salt, encrypted_data, b"")
 
     return str(decrypted_data, "utf-8")
 
@@ -57,17 +57,17 @@ def encrypt_and_encode(master_pass: str, data: str, salt: bytes) -> Union[str,No
     return b64encode(encrypted_data).decode("ascii")
 
 
-def decode_and_decrypt(master_pass: str, data: str, nonce: bytes) -> Union[str,None]:
+def decode_and_decrypt(master_pass: str, data: str, salt: bytes) -> Union[str,None]:
     if not data:
         return ''
 
     ensure_type(master_pass, str, "master_pass", "str")
     ensure_type(data, str, "data", "str")
-    ensure_type(nonce, bytes, "salt", "bytes")
+    ensure_type(salt, bytes, "salt", "bytes")
 
     print(data)
 
-    decrypted_data = decrypt_string(master_pass, b64decode(data), nonce)
+    decrypted_data = decrypt_string(master_pass, b64decode(data), salt)
     return decrypted_data if decrypted_data else ""
 
 
@@ -75,13 +75,13 @@ if __name__ == "__main__":
     master_pass = input("master password: ")
     user_data = input("Input some data you want to encrypt: ")
 
-    nonce = generate_nonce()
-    print(f"Nonce: {nonce}")
+    salt = generate_salt()
+    print(f"salt: {salt}")
 
-    encrypted_and_encoded_data = encrypt_and_encode(master_pass, user_data, nonce)
+    encrypted_and_encoded_data = encrypt_and_encode(master_pass, user_data, salt)
 
     print(encrypted_and_encoded_data)
-    decrypted_data = decode_and_decrypt(master_pass, encrypted_and_encoded_data, nonce)
+    decrypted_data = decode_and_decrypt(master_pass, encrypted_and_encoded_data, salt)
 
     print(decrypted_data == user_data)
 
